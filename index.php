87217718c16ef14e93826ac954314224512eead7
Adashy <Adamigo12345@wp.pl>
2018-12-17 13:52:59 +0100

    Szablon strony HTML

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Formularz</title>
+	<meta charset="utf-8">
+	<meta name="description" content="opis">
+	<meta name="keywords" content="słowa kluczowe">
+	<meta name="author" content="autor">
+	<link rel="icon" type="image/x-icon" href="favicon.ico">
+	<link rel="stylesheet" href="/style/style.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
